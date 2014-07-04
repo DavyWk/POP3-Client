5d6e98e99b7bff1bd73ab7b0061bb2bdf06b02c3
DavyWk <cnetadev@outlook.com>
2014-07-04 12:12:56 -0400

    Connects to outlook.com's POP3 server

---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security;
+
+using Utils;
+using Core.CommandParser;
+
+namespace POP3_Client
+{
+
+    class Program
+    {
+
+        static int Main(string[] args)
+        {
+            const string host = "pop-mail.outlook.com";
+            const int port = 995;
+
+            Console.Title = "POP3 Client";
+
+            TcpClient client = new TcpClient();
+            client.Connect(Dns.GetHostAddresses(host), port);
+
+            if (!client.Connected)
+            {
+                Logger.Network("Failed to connect to {0}", host);
+                Console.ReadKey();
+                return 1;
+            }
+            Logger.Network("Connected to {0}:{1}", host, port);
+
+            using (SslStream s = new SslStream(client.GetStream()))
+            {
+                s.AuthenticateAsClient(host);
+
+                if (!s.IsAuthenticated)
+                {
+                    Console.WriteLine("Error while activating SSL connection");
+                    Console.ReadKey();
+                    return 1;
+                }
+                Logger.Network("SSL connection activated");
+                Logger.Command(Receive(s));
+
+                Console.Write("Enter email address: ");
+                SendCommand(s, string.Format("USER {0}", Console.ReadLine()));
+                Logger.Command(Receive(s));
+
+                Console.Write("Enter password: ");
+                SecureString pw = ReadPassword();
+                SendCommand(s, string.Format("PASS {0}", pw.ToASCIIString()));
+                Logger.Command(Receive(s));
+                pw.Dispose();
+
+                SendCommand(s, "LIST");
+                Logger.Command(Receive(s));
+                ListParser.Display(ListParser.Parse(ReceiveMultiLine(s)));
+
+
+                SendCommand(s, "QUIT");
+                Logger.Command(Receive(s));
+            }
+
+
+
+            Console.ReadKey();
+            return 0;
+        }
+
+        static void SendCommand(Stream s, string command)
+        {
+
+            s.Write(Encoding.ASCII.GetBytes(command + "\r\n"), 0, command.Length + 2);
+        }
+        static string Receive(Stream s)
+        {
+            List<byte> str = new List<byte>();
+            string response;
+
+            while (true)
+            {
+                int b = s.ReadByte();
+                if (b == 10 || b < 0) // \r
+                    break;
+                if (b != 13) // \n
+                    str.Add((byte)b);
+            }
+            response = Encoding.ASCII.GetString(str.ToArray());
+
+            return response;
+        }
+
+        static List<string> ReceiveMultiLine(Stream s)
+        {
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string line = Receive(s);
+
+                if (line == ".")
+                    break;
+
+                list.Add(line);
+            }
+
+            return list;
+        }
+
+        static bool CheckResponse(string s)
+        {
+            if (s.StartsWith("+OK"))
+                return true;
+            else
+                return false;
+        }
+
+        static SecureString ReadPassword()
+        {
+            SecureString s = new SecureString();
+
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (s.Length > 0)
+                    {
+                        s.RemoveAt(s.Length - 1);
+                        Console.Write(key.KeyChar);
+                        Console.Write(" ");
+                        Console.Write(key.KeyChar);
+                    }
+
+                    continue;
+                }
+
+                // accept only letters
+                if ((ConsoleKey.D0 <= key.Key) && (key.Key <= ConsoleKey.Z))
+                {
+                    s.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }
+
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            s.MakeReadOnly();
+
+            return s;
+        }
+
+    }
+
+    public static class Extensions
+    {
+        public static string ToASCIIString(this SecureString s)
+        {
+            IntPtr pointer = Marshal.SecureStringToBSTR(s);
+            string ret = Marshal.PtrToStringBSTR(pointer);
+            Marshal.ZeroFreeBSTR(pointer);
+
+            return ret;
+        }
+    }
+
+}
+
+
+
---
+++ Utils/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class Extensions
+    {
+        public static string Capitalize(this string s)
+        {
+            string ret =  s.Trim();
+            if(string.IsNullOrEmpty(ret))
+                return ret;
+
+            ret = char.ToUpper(ret[0]) + ret.Substring(1);
+            return ret;
+        }
+    }
+}
